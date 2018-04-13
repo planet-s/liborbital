417f2ab325ded3f98a3343d02552c24f119a83f2
Tibor Nagy <xnagytibor@gmail.com>
2018-04-13 18:40:20 +0200

    Simplification, add more functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
 extern crate orbclient;
 
-use orbclient::{Window, Renderer};
+use orbclient::{Window, Renderer, get_display_size};
 use std::os::raw::*;
 use std::mem::transmute;
 use std::ffi::CStr;
+
+#[no_mangle]
+pub unsafe extern "C" fn orb_display_width() -> u32 {
+    get_display_size().unwrap().0
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn orb_display_height() -> u32 {
+    get_display_size().unwrap().1
+}
 
 #[no_mangle]
 pub unsafe extern "C" fn orb_window_new(
@@ -12,32 +22,53 @@ pub unsafe extern "C" fn orb_window_new(
     w: u32,
     h: u32,
     title: *const c_char,
-) ->  *mut c_void {
+) ->  *mut Window {
     let title = CStr::from_ptr(title).to_string_lossy();
-    let window: *mut Window = transmute(Box::new(orbclient::Window::new(x, y, w, h, &title).unwrap()));
-    window as *mut c_void
+    transmute(Box::new(Window::new(x, y, w, h, &title).unwrap()))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn orb_window_data(window: *mut c_void) ->  *mut u32 {
-    let window: &mut Window = &mut *(window as *mut Window);
+pub unsafe extern "C" fn orb_window_data(window: &mut Window) ->  *mut u32 {
     window.data_mut().as_mut_ptr() as *mut u32
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn orb_window_width(window: *mut c_void) -> u32 {
-    let window: &mut Window = &mut *(window as *mut Window);
+pub unsafe extern "C" fn orb_window_width(window: &mut Window) -> u32 {
     window.width()
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn orb_window_height(window: *mut c_void) -> u32 {
-    let window: &mut Window = &mut *(window as *mut Window);
+pub unsafe extern "C" fn orb_window_height(window: &mut Window) -> u32 {
     window.height()
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn orb_window_sync(window: *mut c_void) {
-    let window: &mut Window = &mut *(window as *mut Window);
+pub unsafe extern "C" fn orb_window_x(window: &mut Window) -> i32 {
+    window.x()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn orb_window_y(window: &mut Window) -> i32 {
+    window.y()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn orb_window_set_pos(window: &mut Window, x: i32, y: i32) {
+    window.set_pos(x, y);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn orb_window_set_size(window: &mut Window, w: u32, h: u32) {
+    window.set_size(w, h);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn orb_window_set_title(window: &mut Window, title: *const c_char) {
+    let title = CStr::from_ptr(title).to_string_lossy();
+    window.set_title(&title);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn orb_window_sync(window: &mut Window) {
     window.sync();
 }
--- test/test_01.c
+++ test/test_01.c
@@ -6,14 +6,23 @@
 #define HEIGHT 480
 
 int main() {
-    void *window = orb_window_new(100, 100, WIDTH, HEIGHT, "Test window");
+    void *window = orb_window_new(10, 10, 320, 240, "Test window");
 
-    printf("Window size: %d x %d\n", orb_window_width(window), orb_window_height(window));
+    orb_window_set_pos(window, 100, 100);
+    orb_window_set_size(window, WIDTH, HEIGHT);
 
+    printf("Display size: (%d, %d)\n", orb_display_width(), orb_display_height());
+    printf("Window size: (%d, %d)\n", orb_window_width(window), orb_window_height(window));
+    printf("Window position: (%d, %d)\n", orb_window_x(window), orb_window_y(window));
+
+    char title[1024] = { 0 };
     int frame_count = 0;
     uint32_t *frame_data = orb_window_data(window);
 
     while (true) {
+        sprintf(title, "Frame #%d", frame_count);
+        orb_window_set_title(window, title);
+
         for (int y = 0; y < HEIGHT; ++y) {
             for (int x = 0; x < WIDTH; ++x) {
                 frame_data[y * WIDTH + x] =
